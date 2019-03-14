89fedba0554b8c44a4c79a2e08847e6951d0b406
Sean Gies <seanpgies@gmail.com>
2019-03-14 14:10:51 +0100

    fixed delete_from_set func

--- hittingTree.pl
+++ hittingTree.pl
@@ -1,8 +1,30 @@
 :- [diagnosis].
 
 
+
+%call_prob1(_, _, _, [], [], OUT).
 call_prob1(SD, COMP, OBS, HS, CS, OUT) :-
-    % HS is empty (at start) list of conflict sets
-    diagnosis(SD, COMP, OBS),
-    tp(SD, COMP, OBS, [_|OUT], CS).
-    [OUT|CS].
+    % Calls problem 1 till all conflict sets are gotten.
+    problem1(SD, COMP, OBS),
+    tp(SD, COMP, OBS, [HS|OUT], CS),
+    % TODO: if tp returns false return OUT.
+    call_prob1(SD, COMP, OBS, [HS|CS], CS, [OUT|CS]).
+
+
+call_problem1(SD, COMPS, OBS, HS, CS, OUT, OUT).
+call_problem1(SD, COMPS, OBS, HS, CS, OUT, O) :-
+    problem1(SD, COMP, OBS),
+    append(HS, OUT, Z), % add the outputs together.
+    tp(SD, COMP, OBS, Z, CS),
+
+
+% delete all occurances of a given element from a list
+% Works.
+delSet(_, [], Res, Res).
+delSet(Elem, [X|Xs], Res, R) :-
+    Elem == X,
+    delSet(Elem, Xs, Res, R)
+    ;
+    not(Elem == X),
+    append(Res, [X], Z),
+    delSet(Elem, Xs, Z, R).
